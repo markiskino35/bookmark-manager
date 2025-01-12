3acfa1956d7b038bb86b5115c85a2c77f89a6a05
dentalmystep3 <dental_mystep3@moh.gov.my>
2025-01-12 17:33:50 +0800

    feat: add helmet, mongosanitize, compression

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// CORE
+// CORE ------------------------------------
 import dotenv from 'dotenv';
 import 'express-async-errors';
 
@@ -7,21 +7,56 @@ dotenv.config();
 import express from 'express';
 const app = express();
 
-// IMPORT ROUTER
+// security packages
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+
+// compression packages
+import compression from 'compression';
+
+// IMPORT ROUTER ------------------------------------
 // bookmark import
 import bookmarks from './routes/bookmarks.js';
 
-// IMPORT MIDDLEWARE
+// IMPORT MIDDLEWARE ------------------------------------
 
-// DATABASE
+// DATABASE  ------------------------------------
 import connectDB from './database/connect.js';
 
-// USE MIDDLEWARE
-app.use(express.json());
+// USE MIDDLEWARE ------------------------------------
+// initial config
+app.set('trust proxy', 1);
+app.disable('x-powered-by');
+
+// header
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+    strictTransportSecurity: false,
+    xPoweredBy: false,
+  })
+);
+app.use(async function (req, res, next) {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
+
+// security
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  })
+);
+
+// compression
+app.use(compression());
 
 app.use('/api/bookmarks', bookmarks);
 
-// SERVER
+// SERVER ------------------------------------
 const port = process.env.PORT || 5000;
 
 const start = async () => {
